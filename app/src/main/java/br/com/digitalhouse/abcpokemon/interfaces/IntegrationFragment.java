2588431ca052cc38eb451fe129146e4a2ca187f7
Jair Ribeiro <49173024+jairrb@users.noreply.github.com>
2019-06-10 19:07:36 -0300

    Revert "Jair"

--- app/src/main/java/br/com/digitalhouse/abcpokemon/interfaces/IntegrationFragment.java
+++
@@ -1,7 +0,0 @@
-package br.com.digitalhouse.abcpokemon.interfaces;
-
-import android.support.v4.app.Fragment;
-
-public interface IntegrationFragment {
-    void integration(Fragment fragment);
-}
--- app/src/main/java/br/com/digitalhouse/abcpokemon/splash/SplashActivity.java
+++ app/src/main/java/br/com/digitalhouse/abcpokemon/splash/SplashActivity.java
@@ -1,51 +1,15 @@
 package br.com.digitalhouse.abcpokemon.splash;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.ImageView;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 import br.com.digitalhouse.abcpokemon.R;
-import br.com.digitalhouse.abcpokemon.login.OpcoesLoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    private ImageView splashPokemon;
-    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        splashPokemon = findViewById(R.id.splashPokemon);
-
-        splashPokemon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                jump();
-            }
-        });
-
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                jump();
-            }
-        }, 3000);
     }
-
-    private void jump() {
-        timer.cancel();
-        Intent intent = new Intent(SplashActivity.this, OpcoesLoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
 }
