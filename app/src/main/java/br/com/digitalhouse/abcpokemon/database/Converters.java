48143d7f096a44f3d56ed9194c212899fef2b8f5
ivan <ivan.alineri@outlook.com>
2019-07-15 19:09:57 -0300

    Converters e Database 1.0

--- app/src/main/java/br/com/digitalhouse/abcpokemon/database/Converters.java
+++ app/src/main/java/br/com/digitalhouse/abcpokemon/database/Converters.java
@@ -1,7 +1,9 @@
 package br.com.digitalhouse.abcpokemon.database;
 
 
-import androidx.room.TypeConverter;
+import android.arch.persistence.room.TypeConverter;
+
+
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
